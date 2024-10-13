79e73f0fcfd83a3226a5cf3f07502e38828854f5
Jackshan-Venujan <venujanfast@gmail.com>
2024-10-13 18:36:02 +0530

    Frontend : Developed Landing Page

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Header from './components/layouts/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
