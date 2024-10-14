074fcb4e83e8f80565c08102c8eda98462366fab
Jackshan-Venujan <venujanfast@gmail.com>
2024-10-14 08:48:03 +0530

    Implementing store- haveéto update from redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Header from './components/layouts/Header';
+import {BrowserRouter as Router,Route, Routes} from 'react-router-dom';
+import {HelmetProvider} from 'react-helmet-async';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <HelmetProvider>
+          <Header/>
+            <Routes>
+              <Route path = '/' element = {<Home/>} />
+            </Routes>
+          <Footer/>
+        </HelmetProvider>
+      </div>
+    </Router>
+  
   );
 }
 
